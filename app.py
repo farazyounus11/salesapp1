31476932d2ffef48e909fdb8f27ed90a72d30a46
farazyounus11 <40037013+farazyounus11@users.noreply.github.com>
2026-05-03 21:40:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -62,16 +62,16 @@ if dataset_choice == "Sales Data (sales_data.csv)":
     col4.metric("Avg Order Value ($)", f"{avg_order_value:,.2f}")
 
     # --- Average Sales by Product Line / City / Country ---
-    st.subheader("📊 Average Sales Insights")
+    st.subheader("📊 Sales Insights")
     
     avg_by_productline = (
-        filtered_data.groupby("PRODUCTLINE")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(3)
+        filtered_data.groupby("PRODUCTLINE")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(5)
     )
     avg_by_city = (
-        filtered_data.groupby("CITY")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(3)
+        filtered_data.groupby("CITY")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(5)
     )
     avg_by_country = (
-        filtered_data.groupby("COUNTRY")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(3)
+        filtered_data.groupby("COUNTRY")["SALES"].mean().reset_index().sort_values("SALES", ascending=False).head(5)
     )
     
     col1, col2, col3 = st.columns(3)
